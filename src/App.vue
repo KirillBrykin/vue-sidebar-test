c739b6e75391a7a182f368f96a75118c1d48ccf7
brykin_ki <kir.brykin@yandex.ru>
2023-08-03 23:59:09 +0300

    lesson-1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,100 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import {ref, computed} from "vue";
+import UBotton from "@/components/UBotton.vue";
+
+const numb = 3
+
+const products = ['шавуха', 'чебупели', 'беляш']
+
+const users = [
+  {id: 1, name: 'Джамшут', age: '34'},
+  {id: 2, name: 'Мага', age: '27'},
+  {id: 3, name: 'Шамиль', age: '42'}
+]
+
+const productData = ref('')
+
+const fruitData = ref('')
+
+const fruits = ref(['яблоко', 'ананас', 'груша', 'киви', 'персик', 'банан', 'абрикос'])
+
+const queryFruits = computed(() => {
+  //сравниваем массив символов с помощью indexOf и если этот индекс там есть, то он вернет нам его, в противном случае -1
+  return fruits.value.filter((product) => product.indexOf(fruitData.value) !== -1)
+})
+
+const color = ref('green')
+
+function toggleColor() {
+  color.value = color.value === 'green' ? 'blue' : 'green'
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <nav class="navbar bg-body-tertiary">
+    <form class="container-fluid justify-content-start">
+      <button class="btn btn-outline-success me-2" type="button">Main button</button>
+      <button class="btn btn-sm btn-outline-secondary" type="button">Smaller button</button>
+    </form>
+  </nav>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <p v-if="numb === 4">text 123</p>
+    <p v-else-if="numb ===3">text 456</p>
+    <p v-else>text 789</p>
+  </div>
+
+  <div class="container">
+    <p v-show="numb === 3">text 123</p>
+    <p v-show="numb ===4">text 456</p>
+  </div>
+
+  <div class="container">
+    <ul>
+      <li v-for="product in products">
+        {{ product }}
+      </li>
+    </ul>
+
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }} - {{ user.age }}
+      </li>
+    </ul>
+  </div>
+
+  <div class="container">
+    <p>{{ productData }}</p>
+    <input type="text" v-model="productData">
+  </div>
+
+
+  <div class="container">
+    <p>{{ queryFruits }}</p>
+    <input type="text" v-model="fruitData">
+    <ul>
+      <li v-for="fruit in queryFruits" :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </div>
+
+  <div class="container">
+    <p :style="{ color }" @click="toggleColor">
+      Зеленый текст становится голубым при клике, и обратно
+    </p>
+  </div>
+
+  <div class="container">
+    <UBotton>КНОПКА1</UBotton>
+    <UBotton>КНОПКА2</UBotton>
+  </div>
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/UBotton.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <button class="btn btn-primary mt-4">
+    <!-- slot помещает содержимое компонента где используется UButton -->
+    <slot></slot>
+  </button>
+
+</template>
+
+<style scoped>
+
+</style>
